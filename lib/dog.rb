3ad507337de923b5f1bb4941f9fb8223bf526f89
Jacob Marthaller <marthaller.jr@gmail.com>
2020-11-21 10:26:12 -0500

    cleanup puppy lab for errors

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,19 +7,22 @@ class Dog
 
     def initialize(name)
         @name = name
+        @@all << self
         save
     end
 
     def self.all
-        @@all
+        @@all.uniq
     end
 
     def self.clear_all
         @@all.clear
     end
-
+    
+    #binding.pry
     def self.print_all
-        puts @@all.map { |dog| dog.name }
+        unique = @@all.map { |dog| dog.name }.uniq
+        puts unique
     end
 
     def save 
